7acbd14aba639965bd7554fcb33555d3e240894a
aydin :^) <54159730+aydin-due@users.noreply.github.com>
2021-05-06 16:11:34 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, session
 from werkzeug.utils import redirect
 import secrets, json
 import frases
-import os
+import os 
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
